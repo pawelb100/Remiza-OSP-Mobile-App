d803de3005ff9beb7a6a21b1fb31c8342918e021
Paweł Borucki <pawel.borucki@wseiz.edu.pl>
2022-10-07 15:40:00 +0200

    Implementacja szuflady nawigacyjnej

--- app/src/main/java/edu/wseiz/remizaosp/MainActivity.java
+++ app/src/main/java/edu/wseiz/remizaosp/MainActivity.java
@@ -1,10 +1,18 @@
 package edu.wseiz.remizaosp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -12,20 +20,43 @@ public class MainActivity extends AppCompatActivity {
 
     private FirebaseAuth fAuth;
 
+    private AppBarConfiguration appBarConfiguration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        MaterialToolbar toolbar = findViewById(R.id.topAppBar);
+        setSupportActionBar(toolbar);
 
+        DrawerLayout drawerLayout = findViewById(R.id.DrawerLayout);
+        NavigationView navView = findViewById(R.id.navigationView);
+        appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.dashboardFragment, R.id.firefighterListFragment)
+                .setOpenableLayout(drawerLayout)
+                .build();
+
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_main);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(navView, navController);
+
+/*
         fAuth = FirebaseAuth.getInstance();
         FirebaseUser user = fAuth.getCurrentUser();
 
         if (user == null) {
             startActivity(new Intent(MainActivity.this, LoginActivity.class));
         }
+*/
+    }
 
+    @Override
+    public boolean onSupportNavigateUp() {
+
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_main);
+        return NavigationUI.navigateUp(navController,appBarConfiguration) || super.onSupportNavigateUp();
 
     }
+
 
 }
